35baff390131f74bae8b6d2035e6ddb17139aa12
uma <umap024umasulibele@gmail.com>
2026-07-06 15:02:09 +0530

    new commit

---
+++ variable.py
@@ -0,0 +1,21 @@
+#####variable+datatypes######
+username = "uma"
+followers = "10000"
+verified = True
+rating = 4.8
+
+print(username)
+print(followers)
+print(verified)
+print(rating)
+
+username = input("Enter the username")
+followers = int(input("Enter followers count"))
+verified = input("is acc is verified ? True/False")
+rating = float (input("enter rating"))
+
+print("username:" , username)
+print("followers:" , followers)
+print("verified:" , verified)
+print("rating:" , rating)
+
